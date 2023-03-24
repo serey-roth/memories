7bc8917c3334f5991c91efbc4714ab8eaeaab6df
Serey Roth <sereyratanakroth@gmail.com>
2023-03-23 17:07:16 -0700

    Query posts from database inside loader function in post/index

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,17 +1,29 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import Post from "~/components/Post";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+    const posts = await db.post.findMany();
+    return json({
+        posts
+    });
+}
 
 export default function PostsIndexRoute() {
+    const data = useLoaderData<typeof loader>();
+
     return (
         <div 
         className="flex flex-col w-screen sm:max-w-[500px]">
             <h1 className="font-semibold border-b">Posts</h1>
             <div className="flex flex-col mt-4 gap-2">
-                <Post
-                title="testing"
-                description="lorem ipsum" />
-                <Post
-                title="testing"
-                description="lorem ipsum" />
+               {data.posts.map(post => (
+                    <Post
+                    key={post.id}
+                    title={post.title}
+                    content={post.content || ""} />
+               ))}
             </div>
         </div>
     )
