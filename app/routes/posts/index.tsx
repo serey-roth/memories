c8c7c2d3bc00ce8056801b19a8c9c00050f40c92
Serey Roth <sereyratanakroth@gmail.com>
2023-03-23 20:04:14 -0700

    Add link to posts/new page inside posts/index file and display only the first 10 posts

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,10 +1,14 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { Button } from "flowbite-react";
 import Post from "~/components/Post";
 import { db } from "~/utils/db.server";
 
 export const loader = async () => {
-    const posts = await db.post.findMany();
+    const posts = await db.post.findMany({
+        take: 10,
+        orderBy: { createdAt: "desc" }
+    });
     return json({
         posts
     });
@@ -12,11 +16,21 @@ export const loader = async () => {
 
 export default function PostsIndexRoute() {
     const data = useLoaderData<typeof loader>();
-
+    
     return (
         <div 
         className="flex flex-col w-screen sm:max-w-[500px]">
-            <h1 className="font-semibold border-b">Posts</h1>
+            <div className="flex items-center">
+                <h1 className="font-bold text-xl mr-auto">Posts</h1>
+                <Link to="new">
+                    <Button 
+                    color="light" 
+                    pill={true}
+                    >
+                        New Post
+                    </Button>
+                </Link>
+            </div>
             <div className="flex flex-col mt-4 gap-2">
                {data.posts.map(post => (
                     <Post
