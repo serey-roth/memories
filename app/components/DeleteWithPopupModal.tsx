f4942a2634ea09bafd504d6186d7c947e2181d5f
Serey Roth <sereyratanakroth@gmail.com>
2023-03-28 17:50:53 -0700

    feat(components/DeleteWithPopupModal): create a delete component that opens a modal when the delete button is clicked

---
+++ app/components/DeleteWithPopupModal.tsx
@@ -0,0 +1,61 @@
+import { Button, Modal } from "flowbite-react";
+import { useToggle } from "~/utils/useToggle";
+
+type DeleteWithPopupModalProps = {
+    idToBeDeleted: string;
+    label?: string;
+}
+
+export const DeleteWithPopupModal = ({
+    idToBeDeleted,
+    label = "Delete"
+}: DeleteWithPopupModalProps) => {
+    const { toggleState, toggle } = useToggle();
+
+    return (
+        <>
+            <Button 
+            aria-label="delete"
+            color="failure"
+            onClick={toggle}>
+                {label}
+            </Button>
+            <Modal
+                dismissible={true}
+                show={toggleState}
+                size="md"
+                popup={true}
+                onClose={toggle}
+            >
+                <Modal.Header />
+                <Modal.Body>
+                <div className="text-center">
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                        Are you sure you want to delete this item?
+                    </h3>
+                    <div className="flex justify-center gap-4">
+                    <form
+                    method="post"
+                    action={`/posts/${idToBeDeleted}`}>
+                        <Button
+                            color="failure"
+                            name="intent"
+                            value="delete"
+                            type="submit"
+                        >
+                            Yes, I'm sure
+                        </Button>
+                    </form>
+                    <Button
+                        color="gray"
+                        onClick={toggle}
+                    >
+                        No, cancel
+                    </Button>
+                    </div>
+                </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
